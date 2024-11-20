9b53dbe0f657430d37b9fd546e1b766d1ea42871
Siyabonga Samson Lukhele <samsonlukhele76@gmail.com>
2024-11-20 16:07:22 +0200

    Explain difference between "optional" and "default"

--- 0x03-functions/0x02-optional-parameters.ts
+++ 0x03-functions/0x02-optional-parameters.ts
@@ -33,3 +33,18 @@ const calculate = (
 
   throw new Error("Operation not supported currently");
 };
+
+// Diffs between the two functions
+
+// #1. `age?` indicates that the age is optional and the function can work even in cases where age is not provided. Caution has to be taken though when this is the case
+// #2. `operation: string = "add"` indicates that the default operation is to add a and b. If another operation is provided, we check if it's available.
+
+// The main difference?
+/**
+ * When running function 1 if age is not optional the function may throw an error
+ * However, indicating that it is optional means you can run the function even when age is not provided.
+ * Again, handling has to be made here.
+ *
+ * In function 2 however, the operation argument is not optional but it is defaulted to "add" meaning an operation is available by default.
+ * This means, if we leave operation blank the function already has a default operation it picked for us
+ */
